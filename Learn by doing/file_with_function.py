159a0de5a49e15871ca97081fbd41fd9c3dc63a9
Namitjain <39775824+NamitJain-Analytics@users.noreply.github.com>
2026-05-17 16:51:57 +0530

    Add files via upload

---
+++ Learn by doing/file_with_function.py
@@ -0,0 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.2
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+def area_of_rectangle(length , width):
+    print('value of __name__ is',__name__)
+    area = length*width
+    return area
